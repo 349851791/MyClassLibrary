3caa6e9d11461c5ea00547d8cded066904688f3c
349851791 <349851791@qq.com>
2017-02-20 13:35:27 +0800

    增加左侧目录树图标 增加IP页面

---
+++ DoNet.Utils.DemoWeb/WebForms/ASPDemo/GetIP.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DoNet.Utils.DemoWeb.WebForms.ASPDemo
+{
+    public partial class GetIP : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string AddressIP = string.Empty;
+                foreach (IPAddress _IPAddress in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+                {
+                    if (_IPAddress.AddressFamily.ToString() == "InterNetwork")
+                    {
+                        AddressIP = _IPAddress.ToString();
+                        fwqIP.Text ="服务器IP地址为:"+ AddressIP;
+                        khdIP.Text += "客户端IP地址为:" + System.Web.HttpContext.Current.Request.UserHostAddress ; 
+                        break;
+                    }
+                } 
+            }
+        }
+    }
+}
